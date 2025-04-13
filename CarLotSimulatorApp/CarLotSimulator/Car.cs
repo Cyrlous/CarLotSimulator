13c7436bb898d24cdf8903f7e238a44f10f8d95f
Daniel Page <dpage105@gmail.com>
2025-04-13 17:59:20 -0500

    Created Car class file

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Dynamic;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    public Car()
+    {
+        
+    }
+
+    public int Year { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string EngineNoise { get; set; }
+    public string HonkNoise { get; set; }
+
+    public void MakeEngineNoise(string engineNoise)
+    {
+        Console.WriteLine(engineNoise);
+    }
+
+    public void MakeHonkNoise(string honkNoise)
+    {
+        Console.WriteLine(honkNoise);
+    }
+}
